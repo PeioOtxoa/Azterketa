2923629551cecb3e6260b8946ee88d725a82fe7d
Ikaslea <p_otxoa@fpzornotza.com>
2022-03-02 14:31:37 +0100

    azterketa commit

---
+++ DibujarPoligono/src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import base.Poligono;
+
+public class test {
+
+
+	void testUnitario() {
+		Poligono base = new Poligono(null);
+		int numero = 10;
+		int resultado = base.getLados();
+	}
+}
